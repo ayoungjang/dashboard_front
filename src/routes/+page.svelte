6a0b2d2d0594c48fc8c09018ce4c0262a9b89370
ayoungjang <aja134@uit.no>
2024-06-17 19:30:34 +0200

    feat:init

---
+++ src/routes/+page.svelte
@@ -0,0 +1,21 @@
+<div>
+	<div>Dashboard</div>
+	<div>
+		<div class="nav-bar">
+			<div>
+				<select>
+					<option> Strip </option>
+					<option> Disk </option>
+				</select>
+				<div>
+					<input type="file" />
+				</div>
+				<div>
+					<input type="file" />
+				</div>
+			</div>
+			<div>Excute</div>
+		</div>
+		<div></div>
+	</div>
+</div>
